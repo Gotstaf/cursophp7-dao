5d0e6a2485371de7b6f492b4e8559bf69ee5d999
Gotstaf <41273411+Gotstaf@users.noreply.github.com>
2020-07-14 07:43:57 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadbyId(2);
+//$root = new Usuario();
+//$root->loadbyId(2);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario -> login("root", "")
+
+echo $usuario;
 
 ?>
